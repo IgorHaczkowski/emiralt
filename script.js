16341be3efdd285279f241b4b7bba372aee8abbe
IgorHaczkowski <57094189+IgorHaczkowski@users.noreply.github.com>
2020-05-12 16:24:29 +0200

    działaj

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').bxSlider({
+        pager: false,
+        controls: false,
+        auto: true,
+        mode: 'fade',
+        wrapperClass: 'slider-wrapper'
+    });
+});
